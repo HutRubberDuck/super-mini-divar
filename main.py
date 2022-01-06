d3d697353d2232ec2d14cba44a41c99841332d3f
Behrad Ravanbod <99behi@gmail.com>
2022-01-07 01:42:51 +0330

    add Dockerfile and docker-compose file and DB_URI env.

--- main.py
+++ main.py
@@ -1,12 +1,8 @@
+from os import environ
 from models import Database
 
-DATABASE_URL = ""
-
 # Connecting to database
-db = Database(DATABASE_URL)
+db = Database(environ['SUPER_MINI_DIVAR_DB_URL'])
 db.create_database()
 
 dbsession = db.session_local # You can use this object to create queries
-
-
-
