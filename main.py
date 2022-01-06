2c7ed5477baf4c1b6b9d46c64735ddca52cb1f56
Behrad Ravanbod <99behi@gmail.com>
2022-01-07 03:23:05 +0330

    add city, district and province model. add some routes to show provinces. add fastapi.

--- main.py
+++ main.py
@@ -1,8 +1,23 @@
 from os import environ
-from models import Database
+from models import Database, Province, City
+from fastapi import FastAPI
 
 # Connecting to database
-db = Database(environ['SUPER_MINI_DIVAR_DB_URL'])
+db = Database(environ['SUPER_MINI_DIVAR_DB_URI'])
 db.create_database()
 
 dbsession = db.session_local # You can use this object to create queries
+
+
+app = FastAPI()
+
+@app.get("/province/")
+def get_all_provinces():
+    items = dbsession.query(Province).all()
+    return items
+
+
+@app.get("/province/{province_id}/")
+def get_province_by_id(province_id: int):
+    province = dbsession.query(Province).filter(Province.id == province_id).first()
+    return province
--- models/__init__.py
+++ models/__init__.py
@@ -1,4 +1,7 @@
 from models.database import Database, Base
 
 from models.example import Example
+from models.province import Province
+from models.city import City
+from models.district import District
 
