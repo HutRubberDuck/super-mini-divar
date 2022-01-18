b1efa2811d6aa22be24da7b00e8dbd7f4cb9026e
Amin Rasouli <me@aminrasouli.ir>
2022-01-18 22:35:25 +0330

    added debug for realod uvicorn and docker compose

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import uvicorn
 
-from src.core.settings import APP_HOST, APP_PORT
+from src.core.settings import APP_HOST, APP_PORT, DEBUG
 from src.main import app
 
 if __name__ == "__main__":
-    uvicorn.run(app, host=APP_HOST, port=APP_PORT)
+    uvicorn.run(app, host=APP_HOST, port=APP_PORT, reload=DEBUG)
